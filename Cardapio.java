c4d6c9a9121e1a8e7063aaf72b4fa951d9b4401f
Rodrigo Salvador <rodrigossalve@gmail.com>
2026-06-06 10:37:35 -0300

    Adicao do edital em todas as classes

--- Cardapio.java
+++ Cardapio.java
@@ -1,9 +1,15 @@
-
 /**
- * Escreva uma descrição da classe Cardapio aqui.
- * 
- * @author (seu nome) 
- * @version (um número da versão ou uma data)
+ * O gerenciamento dos pratos deve ser feito na classe Cardapio que também deve ser criada. 
+ * Nesta classe, serão armazenados os pratos do restaurante. Crie um construtor que recebe a quantidade de pratos que o restaurante possui e cria o cardápio (vetor de pratos – você define quantos pratos haverá no cardápio). 
+ * Além do construtor, esta classe deve ter os seguintes métodos:
+ * a) adicionarPrato: insere um objeto prato no vetor e para isto, recebe o objeto prato e retorna true se a inserção pode ser realizada ou false se a inserção não foi possível;
+ * b) removerPrato: recebe o nome do prato e, caso ele exista, remove o prato do cardápio, reorganiza os objetos e retorna true se a remoção foi realizada com sucesso ou false se a remoção não foi possível;
+ * c) buscarPratoPorNome: recebe o nome do prato e retorna o prato que possui este nome;
+ * d) buscarPosicao: recebe o prato e retorna a posição do vetor em que este prato está armazenado. Caso não esteja armazenado, retorna o valor -1;
+ * e) buscarPratoIngrediente: recebe o nome do prato e retorna um vetor com os pratos em que este ingrediente faça parte;
+ * f) buscarValorCardapio: retorna o somatório do valor de todos os pratos do restaurante;
+ * g) buscarPratoEconomico: retorna o prato que possui o menor valor;
+ * h) mostrarPratos: mostra os dados dos pratos do cardápio.
  */
 public class Cardapio
 {
--- Prato.java
+++ Prato.java
@@ -1,9 +1,17 @@
-
-/**
- * Escreva uma descrição da classe Prato aqui. 
- * 
- * @author (VASCO)
- * @version (06.02.2026)
+/** 
+ * Criar, também, uma classe Prato que terá os seguintes atributos: 
+ * nome (String), 
+ * valor (double), 
+ * vetIngredientes (vetor de Ingredientes)
+ * index (int) que servirá para gerenciar as inserções e remoções dos ingredientes no vetIngredientes. 
+ * O construtor desta classe deverá receber como parâmetro o nome, o valor do prato e a quantidade de ingredientes que o prato possui. 
+ * Ainda no construtor devem ser atribuídos os valores aos atributos, inicializar o atributo index e instanciar o vetor de ingredientes (vetIngredientes) com a quantidade de ingredientes que o prato possui. 
+ * Crie os demais métodos básicos necessários para a classe (estejam atentos ao atributo index que apenas a classe deve ter acesso a ele e ao atributo veIngredientes que terá seus próprios métodos de acesso, relacionados abaixo). 
+ * Além do construtor e dos métodos básicos, a classe Prato deve possuir os seguintes métodos:
+ * a) adicionarIngrediente que recebe um Ingrediente e, se houver espaço, adiciona-o ao vetor de ingredientes, retorna true se a inserção pode ser realizada ou false se a inserção não foi possível;
+ * b) consultarIngrediente que recebe o nome do ingrediente, procura-o no vetor de ingredientes e o retorna;
+ * c) removerIngrediente que recebe o nome do ingrediente, consulta-o no vetor de ingredientes e, ao encontrá-lo, o remove do vetor de ingredientes, fazendo a reorganização do vetor de ingredientes. Retorna true se a remoção foi realizada com sucesso ou false se a remoção não foi possível;
+ * d) buscarPosicao que recebe o nome do ingrediente e retorna a posição em que ele está armazenado no vetor de ingredientes. Caso não esteja armazenado, retorna -1;
  */
 public class Prato {
     private String nome;
